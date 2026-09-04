bc7bcd717f9eddd29156051183afcbe31f672f37
take915switch-ux <take915switch@gmail.com>
2026-09-05 07:40:48 +0900

    Add service worker for optical ray lab

---
+++ optical-ray-lab/sw.js
@@ -0,0 +1,5 @@
+const CACHE='optical-ray-lab-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match('./index.html'))))});
